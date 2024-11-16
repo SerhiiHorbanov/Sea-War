1dbbc36693b84cb1ea97cc3cfd463e629444dc38
Serhii <serikgorbanev@gmail.com>
2024-11-16 10:52:56 +0100

    Put initialization and game loop in main
    For now game loop runs for ever

--- Sea War/main.cpp
+++ Sea War/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "Initialization.h"
+#include "Input.h"
+#include "Render.h"
+#include "Update.h"
 
 int main()
 {
+    Initialization();
+
+    while (true)
+    {
+        Render();
+        Input();
+        Update();
+    }
+
     return 0;
 }
